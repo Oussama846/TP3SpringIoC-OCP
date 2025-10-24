dba3bb72a8860614ae18fd4a463b72f30cdc07f0
Oussama846 <oussamadarradirma@gmail.com>
2025-10-24 17:21:26 +0200

    Add files via upload

---
+++ TP3-main/src/main/java/org/example/dao/DaoImpl.java
@@ -0,0 +1,11 @@
+package org.example.dao;
+
+// dao/DaoImpl.java (100.0, profil prod)
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("dao")
+@Profile("prod")
+public class DaoImpl implements IDao {
+    @Override public double getValue(){ return 100.0; }
+}
